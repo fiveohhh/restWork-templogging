94b9e4dad14344414f98047a71f2593548a4fbeb
Andy <andy@chiefmarley.com>
2011-12-24 01:10:50 -0500

    Updated status view for temp display
    Now only finds latest measurement of each sensor number.

--- status/views.py
+++ status/views.py
@@ -5,18 +5,31 @@ from django.template import Context, loader
 import datetime
 
 def index(request):
-    lastTemps = Temp_entry.objects.order_by('dateTime').reverse()[:2]
-    thisWork = str(lastTemps)
+    # Get distinct sensor values
+    distinctSensorVals = Temp_entry.objects.values('sensor').distinct()
+    
+    lastTemps = []
+    
+    # Get last entry for each one of our distict sensor values
+    for sensor in distinctSensorVals:
+        sensorHistory = Temp_entry.objects.filter(sensor=sensor['sensor'])
+        lastEntry = sensorHistory.order_by('dateTime').reverse()[0]
+        lastTemps.append(lastEntry)
 
+    
     temps = []    
-
+    
+    # Put into readable string
     for t in lastTemps:
         name = 'Unknown: '
         if t.sensor == 48:
             name = 'Garage: '
         elif t.sensor == 49:
             name = 'Outside: '
+        elif t.sensor == 50:
+            name = 'Kitchen: ' 
         
+        #TODO Format these strings so they look decent on the web page
         temp = str(((t.temp/100) - 273.15) * 1.8 + 32) + 'F   Last Updated:'
         updated = str(datetime.datetime.fromtimestamp(t.dateTime))
         out = name + temp + updated
