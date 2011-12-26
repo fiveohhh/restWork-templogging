fef53a078377fccc1c8d01ba7b988a0d279a2a32
Andy <andy@chiefmarley.com>
2011-12-25 21:52:12 -0500

    Created model for door status and url for generic msgs

--- restInterface/models.py
+++ restInterface/models.py
@@ -15,3 +15,19 @@ class Temp_entry(models.Model):
         return ('GMT: ' + str(datetime.datetime.fromtimestamp(self.dateTime)) + '\n' +
                 'Sensor: ' + str(self.sensor) + '\n' +
                 'Temp: ' + str(self.temp) + '\n')
+
+class door_status:
+    doorNumber = models.IntegerField()  # Door that reported a status change
+    isOpen = models.IntegerField()      # 0=closed 1=open
+    dateTime = models.IntegerField()    # datetime as unixepoch gmt
+
+    def create(date, doorNum, isOpe):
+        print isOpe
+        return door_status(dateTime = date, doorNumber = doorNum, isOpen = isOpe)
+    create = staticmethod(create)
+
+    def __unicode__(self):
+        return ('GMT: ' + str(datetime.datetime.fromtimestamp(self.dateTime)) + '\n' +
+                'Door#: ' + str(doorNumber) + '\n' +
+                'isOpen: ' + str(isOpen) + '\n')
+                 
