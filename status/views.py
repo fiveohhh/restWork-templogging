9f4b9764a9f42770c74ccdf6bd4bea316b4ccc94
Andy <andy@chiefmarley.com>
2011-12-27 22:51:40 -0600

    Added graphs for outdoors in kitchen
    Using Google Chart API's to make graph for the previous 24 hours.

--- status/views.py
+++ status/views.py
@@ -4,6 +4,18 @@ from restInterface.models import Temp_entry, door_entry, hvac_runtime
 from django.template import Context, loader
 import datetime
 
+
+def getHistoryToGraph(querySetOfTemp_entry):
+    historyCnt = range(290)#we want the last 72*4 entries
+    retArray = []
+    for i in historyCnt:
+        if i % 4 == 0:
+            temp =  str(((querySetOfTemp_entry[i].temp/100.0) - 273.15) * 1.8 + 32) 
+            retArray.append(temp)
+    retArray.reverse() 
+    return retArray #oldest first
+
+
 def index(request):
    
     ############ Get temps ###############
@@ -19,10 +31,13 @@ def index(request):
         lastTemps.append(lastEntry)
     
     temps = []    
-    
-    # Put into readable string
+      # Put into readable string
     for t in lastTemps:
         tempEntry = {}
+        
+        #TODO Format these strings so they look decent on the web page
+        tempEntry['temp'] = str(((t.temp/100.0) - 273.15) * 1.8 + 32) + 'F' 
+        tempEntry['updated'] = str(datetime.datetime.fromtimestamp(t.dateTime))
         tempEntry['name'] = ''
         if t.sensor == 48:
             tempEntry['name'] += 'Garage: '
@@ -32,13 +47,34 @@ def index(request):
             tempEntry['name'] += 'Kitchen: ' 
         else:
             tempEntry['name'] += 'Unknown: '
-        #TODO Format these strings so they look decent on the web page
-        tempEntry['temp'] = str(((t.temp/100.0) - 273.15) * 1.8 + 32) + 'F' 
-        tempEntry['updated'] = str(datetime.datetime.fromtimestamp(t.dateTime))
         temps.append(tempEntry)
+   
     ############# END Get temps ###############
 
 
+ 
+    ######### Get history to graph###############
+    # out of the last 24 hours(24*(12 readings an hour=288entris))
+    # take every 4th point (72 total) for the sensors you want
+    # on the main status page, this stuff will all be hardcoded since
+    # since we only want specific vals to display
+    #########
+    # get outside temps, newest entries first
+    revOutsideTempHistory = Temp_entry.objects.filter(sensor=49).order_by('dateTime').reverse()
+    outsideTemps = getHistoryToGraph(revOutsideTempHistory)
+    
+    revInsideTempHistory = Temp_entry.objects.filter(sensor=50).order_by('dateTime').reverse()
+    kitchenTemps = getHistoryToGraph(revInsideTempHistory)
+    print str(len(kitchenTemps))
+    plotPointList = []
+    historyCnt = range(72)
+    for i in historyCnt:
+        plotPoint = {} 
+        plotPoint['kitchen'] = kitchenTemps[i]
+        plotPoint['outside'] = outsideTemps[i]
+        plotPoint['id'] = i / 3.0 
+        plotPointList.append(plotPoint)
+   
     ############ Get doors ####################
     distinctDoorVals = door_entry.objects.values('doorNumber').distinct()
     
@@ -84,6 +120,7 @@ def index(request):
         'temps' : temps,
         'doors' : doors,
         'hvac_usage' : hvac_usage,
+        'plotPointList' : plotPointList,
     })
 
     return HttpResponse(t.render(c))
