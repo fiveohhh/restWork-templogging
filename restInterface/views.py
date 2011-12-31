7c9677b4377afbb05cb7400165f5c9833518babe
Andy <andy@chiefmarley.com>
2011-12-30 21:38:51 -0600

    Updated to handle logging interval on webserver
    The less work I put on the megaServer, the less dependent dependent I am on it.  I would like to have it eventually just pass everything through

--- restInterface/views.py
+++ restInterface/views.py
@@ -1,13 +1,27 @@
 from django.http import HttpResponse
 from restInterface.models import Temp_entry, door_entry
+from django.db.models import Max
 import time
+import datetime
+
+MINIMUM_SECONDS_BETWEEN_TEMP_LOGGING = 60 * 9
+
 
 def insert(request, s, t, d):
     # override incoming time with current server time github issue#2
     currentTime = round(time.time())
-    te = Temp_entry.create(int(currentTime), int(s), int(t))
-    te.save()
-    retString = s + ' temp:' + t + ' date:' + d
+    lastEntryForSensor = Temp_entry.objects.filter(sensor=s).aggregate(Max('dateTime'))
+    lastTimeEntryOccured = lastEntryForSensor['dateTime__max']
+   
+    retString = ''
+ 
+    secondsSinceLastEntry = currentTime - lastTimeEntryOccured
+    if secondsSinceLastEntry >= MINIMUM_SECONDS_BETWEEN_TEMP_LOGGING:  
+        te = Temp_entry.create(int(currentTime), int(s), int(t))
+        te.save()
+        retString += '{Succesful:Logged:' + t + ' to sensor:' + s + '}'
+    else:
+        retString += '{Msg:Not Logged, last entry was ' + str(secondsSinceLastEntry) + ' seconds ago}' 
     return HttpResponse(retString)
 
 def processMsg(request, msg):
